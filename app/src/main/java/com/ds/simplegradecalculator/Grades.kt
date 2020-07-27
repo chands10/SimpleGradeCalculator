18a6d75107551dea14195104b3d441b0152f0907
Salil Chandra <chands10@rpi.edu>
2020-07-26 22:19:18 -0400

    Implemented functions

--- app/src/main/java/com/ds/simplegradecalculator/Grades.kt
+++ app/src/main/java/com/ds/simplegradecalculator/Grades.kt
@@ -1,18 +1,20 @@
 package com.ds.simplegradecalculator
 // [rawGrades] with category as key and weight as value
-class Grades(val rawGrades: Map<String, Double>) {
+class Grades(rawGrades: Map<String, Double>) {
     // category named [name] and with percentage of grade [weighting]
     class Category(val name: String, val weighting: Double) {
         val scores = mutableListOf<Double>()
     }
 
-    val grades = rawGrades.mapValues { Category(it.key, it.value) }
+    private val grades = rawGrades.mapValues { Category(it.key, it.value) }
 
-    fun addScores(category: String, scores: List<Double>) {
-        TODO()
-    }
+    // add a list of [scores] to [category] and return true if successful
+    fun addScores(category: String, scores: List<Double>) = grades[category]?.scores?.addAll(scores)
 
-    fun calculateGrade() {
-        TODO()
+    // calculate the total grade and return it
+    fun calculateGrade(): Double {
+        var total = 0.0
+        for (g in grades) total += g.value.scores.sum() * g.value.weighting
+        return total
     }
 }
