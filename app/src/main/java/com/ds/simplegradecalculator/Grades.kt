b6009e27b1547783f53dfdb4d5251413d2c6d13f
Salil Chandra <chands10@rpi.edu>
2020-07-27 22:26:47 -0400

    Improved implementation of calculateGrade()

--- app/src/main/java/com/ds/simplegradecalculator/Grades.kt
+++ app/src/main/java/com/ds/simplegradecalculator/Grades.kt
@@ -1,20 +1,30 @@
 package com.ds.simplegradecalculator
 // [rawGrades] with category as key and weight as value
-class Grades(rawGrades: Map<String, Double>) {
-    // category named [name] and with percentage of grade [weighting]
-    class Category(val name: String, val weighting: Double) {
+class Grades(private val rawGrades: Map<String, Double>) {
+    // category with percentage of grade [weighting] and test grades [scores]
+    class Category(val weighting: Double) {
         val scores = mutableListOf<Double>()
     }
+    private val c = checkRep()
+    private val grades = rawGrades.mapValues { Category(it.value) }
 
-    private val grades = rawGrades.mapValues { Category(it.key, it.value) }
+    @Throws(RuntimeException::class)
+    private fun checkRep() {
+        if (rawGrades.isNotEmpty() &&  rawGrades.values.sum() != 1.0) {
+            throw RuntimeException("Weighting must be equal to 1")
+        }
+    }
 
-    // add a list of [scores] to [category] and return true if successful
+    // add a list of [scores] to [category] and return true if successful else return false/null
     fun addScores(category: String, scores: List<Double>) = grades[category]?.scores?.addAll(scores)
 
     // calculate the total grade and return it
     fun calculateGrade(): Double {
-        var total = 0.0
-        for (g in grades) total += g.value.scores.average() * g.value.weighting
-        return total
+        //update weightings to account for empty categories
+        val trueGrades = grades.filter { it.value.scores.isNotEmpty() }
+        val w = trueGrades.map { it.value.weighting }.sum()
+
+        if (w == 0.0) return 100.0 // grades are empty or all categories of grades are empty
+        return trueGrades.map { it.value.scores.average() * it.value.weighting / w }.sum()
     }
 }
