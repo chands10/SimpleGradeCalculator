d7f910b70cfd7db45d463d95d0cc574224e7b186
Salil Chandra <chands10@rpi.edu>
2020-08-04 21:36:39 -0400

    Converted list to RecycleView, fixed list bugs, added TextChangedListener

--- app/src/main/java/com/ds/simplegradecalculator/MainActivity.kt
+++ app/src/main/java/com/ds/simplegradecalculator/MainActivity.kt
@@ -1,44 +1,44 @@
 package com.ds.simplegradecalculator
 
-import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ArrayAdapter
 import android.widget.EditText
 import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    // used for Add Another button
-    val cats = mutableListOf(true)
-    private var adapter: CategoryAdapter? = null
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        adapter = CategoryAdapter(this, cats)
-        categories_list.adapter = adapter
+        categories_list.apply {
+            layoutManager = LinearLayoutManager(this@MainActivity)
+            adapter = CategoryAdapter()
+        }
     }
 
     // Add Another button: adds another field to input text
     fun addCategory(view: View) {
-        cats.add(true)
-        adapter?.notifyDataSetChanged()
+        CategoryContent.addItem()
+        categories_list.adapter?.notifyDataSetChanged()
     }
 
     // Continue button: Verifies scores, creates Grades class, and continues onto the next page
     // TODO: Pass Grades g into next activity
     fun createGrades(view: View) {
         val rawGrades = mutableMapOf<String, Double>()
-        for (i in 0 until cats.size) {
+        for (i in 0 until CategoryContent.size()) {
             val current = categories_list.getChildAt(i)
             val category = current.findViewById<EditText>(R.id.editTextCategory)
             val weight = current.findViewById<EditText>(R.id.editTextWeight)
             if (checkGrade(category, weight, rawGrades)) {
-                rawGrades.put(category.text.toString(), weight.text.toString().toDouble())
+                rawGrades[category.text.toString()] = weight.text.toString().toDouble()
             } else {
                 rawGrades.clear()
                 break
@@ -70,13 +70,62 @@ class MainActivity : AppCompatActivity() {
         return true
     }
 
-    // List item for each category and weight pair
-    private class CategoryAdapter(context: Context, cats: MutableList<Boolean>): ArrayAdapter<Boolean>(context, 0, cats) {
-        override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
-            if (convertView == null) {
-                return LayoutInflater.from(context).inflate(R.layout.list_item, parent, false)
-            }
-            return convertView
+    private class CategoryAdapter: RecyclerView.Adapter<CategoryAdapter.ViewHolder>() {
+
+        override fun onCreateViewHolder(
+            parent: ViewGroup,
+            viewType: Int
+        ): CategoryAdapter.ViewHolder {
+            val view = LayoutInflater.from(parent.context).inflate(R.layout.list_item, parent, false)
+            return ViewHolder(view)
         }
+
+        override fun onBindViewHolder(holder: CategoryAdapter.ViewHolder, position: Int) {
+            val item = CategoryContent.ITEMS[position]
+            holder.mCategoryLabel.setText(item.category)
+            holder.mWeightLabel.setText(item.weight)
+        }
+
+        override fun getItemCount(): Int {
+            return CategoryContent.size()
+        }
+
+        inner class ViewHolder(view: View): RecyclerView.ViewHolder(view) {
+            val mCategoryLabel = view.findViewById<EditText>(R.id.editTextCategory)
+            val mWeightLabel = view.findViewById<EditText>(R.id.editTextWeight)
+
+            init {
+                mCategoryLabel.addTextChangedListener(object: TextWatcher {
+                    override fun afterTextChanged(p0: Editable?) {}
+                    override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+                    override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                        CategoryContent.ITEMS[adapterPosition].category = mCategoryLabel.text.toString()
+                    }
+                })
+
+                mWeightLabel.addTextChangedListener(object: TextWatcher {
+                    override fun afterTextChanged(p0: Editable?) {}
+                    override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+                    override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                        CategoryContent.ITEMS[adapterPosition].weight = mWeightLabel.text.toString()
+                    }
+                })
+            }
+        }
+    }
+
+    object CategoryContent {
+        val ITEMS = mutableListOf<CategoryItem>()
+        init {
+            addItem()
+        }
+
+        fun addItem() {
+            ITEMS.add(CategoryItem("", ""))
+        }
+
+        fun size() = ITEMS.size
+
+        data class CategoryItem(var category: String, var weight: String)
     }
 }
