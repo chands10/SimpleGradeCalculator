0277f16ce9a084053b4aca8a7ff1c67056c7f8ae
Salil Chandra <chands10@rpi.edu>
2020-08-20 12:22:41 -0400

    Added null parameter option for functions, minor layout change

--- app/src/main/java/com/ds/simplegradecalculator/Grades.kt
+++ app/src/main/java/com/ds/simplegradecalculator/Grades.kt
@@ -29,15 +29,15 @@ class Grades(private val rawGrades: Map<String, Double>): Serializable {
     // add a list of [scores] to [category]
     // return true if successful else return false if list does not change and null otherwise
     // TODO: Might delete function and convert scores to immutable list if previous button is implemented on page 2
-    fun addScores(category: String, scores: List<Double>) = grades[category]?.scores?.addAll(scores)
+    fun addScores(category: String?, scores: List<Double>) = grades[category]?.scores?.addAll(scores)
 
     // return an immutable list of scores of a [category] to be viewed
-    fun getScores(category: String) = grades[category]?.scores?.toList()
+    fun getScores(category: String?) = grades[category]?.scores?.toList()
 
     // copy [scores] and set them to the grades class with key [category]
     // return null if category does not exist, false if list is empty, and true otherwise
     // (return may be removed in future if addScores is deleted)
-    fun setScores(category: String, scores: List<Double>): Boolean? {
+    fun setScores(category: String?, scores: List<Double>): Boolean? {
         grades[category]?.scores?.clear()
         return addScores(category, scores)
     }
